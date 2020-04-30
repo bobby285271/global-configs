9f428648aa171319ebf2e4e70ecd7e9c8d7c588d
Bobby Rong <rjl931189261@126.com>
2020-04-30 23:10:45 +0800

    Create a.cpp

---
+++ acm/home/bobby285271/a.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+
+// ll a[200010];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll T;
+    cin >> T;
+    while (T--)
+    {
+        ll n;
+        cin >> n;
+
+        // memset(a, 0, sizeof(a));
+        // for (ll i = 0; i < n; i++)
+        // {
+        //     cin >> a[i];
+        // }
+
+        // bool flag = 1;
+
+        // for (ll i = 0; i < n; i++)
+        // {
+        //     /* code */
+        // }
+
+        // if (flag == 1)
+        // {
+        //     cout << "Yes" << endl;
+        // }
+        // else
+        // {
+        //     cout << "No" << endl;
+        // }
+    }
+    return 0;
+}
