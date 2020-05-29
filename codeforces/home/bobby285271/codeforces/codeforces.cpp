f676cd970b99383a1a9a37700e7523a575bf4e1e
Bobby Rong <rjl931189261@126.com>
2020-05-29 21:30:53 +0800

    Update codeforces.cpp

--- codeforces/home/bobby285271/codeforces/codeforces.cpp
+++ codeforces/home/bobby285271/codeforces/codeforces.cpp
@@ -1,37 +1,26 @@
 #include <bits/stdc++.h>
 #pragma GCC optimize("Ofast")
-using namespace std;
-
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-
-const int INF = 0x3f3f3f3f;
-const int MAXN = 2e5 + 10;
-const int MOD = 1e9 + 7;
-
+#define _USE_MATH_DEFINES
 #define endl '\n'
 #define pb push_back
 #define fir first
 #define sec second
-
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+const int INF = 0x3f3f3f3f;
+const int MAXN = 2e5 + 10;
+const int MOD = 1e9 + 7;
+long long TESTCASE_NUM = 1;
 template <class T>
-void _read(T &x)
-{
-    cin >> x;
-}
+void _read(T &x) { cin >> x; }
 void _read(int &x) { scanf("%d", &x); }
-void _read(ll &x) { scanf("%lld", &x); }
+void _read(long long &x) { scanf("%lld", &x); }
 void _read(float &x) { scanf("%f", &x); }
 void _read(double &x) { scanf("%lf", &x); }
 void _read(char &x) { x = getchar(); }
 void _read(char *x) { scanf("%s", x); }
-template <class T, class U>
-void _read(pair<T, U> &x)
-{
-    _read(x.first);
-    _read(x.second);
-}
 void read() {}
 template <class T, class... U>
 void read(T &head, U &... tail)
@@ -39,32 +28,14 @@ void read(T &head, U &... tail)
     _read(head);
     read(tail...);
 }
-
 template <class T>
-void _write(const T &x) { cout << x << " "; }
-void _write(const int &x) { printf("%d ", x); }
-void _write(const ll &x) { printf("%lld ", x); }
-void _write(const float &x) { printf("%.6f ", x); }
-void _write(const double &x) { printf("%.8lf ", x); }
-void _write(const char &x)
-{
-    putchar(x);
-    if (x != '\n')
-        putchar(' ');
-}
-void _write(const char *x) { printf("%s ", x); }
-template <class T, class U>
-void _write(const pair<T, U> &x)
-{
-    _write(x.first);
-    _write(x.second);
-}
-template <class T>
-void _write(const vector<T> &x)
-{
-    for (auto i = x.begin(); i != x.end(); _write(*i++))
-        ;
-}
+void _write(const T &x) { cout << x; }
+void _write(const int &x) { printf("%d", x); }
+void _write(const long long &x) { printf("%lld", x); }
+void _write(const float &x) { printf("%.6f", x); }
+void _write(const double &x) { printf("%.8lf", x); }
+void _write(const char &x) { putchar(x); }
+void _write(const char *x) { printf("%s", x); }
 void write() {}
 template <class T, class... U>
 void write(const T &head, const U &... tail)
@@ -72,26 +43,10 @@ void write(const T &head, const U &... tail)
     _write(head);
     write(tail...);
 }
-ll TESTCASE_NUM = 1;
-inline ll qpow(ll x, ll n, ll m)
-{
-    ll res = 0;
-    while (n)
-    {
-        if (n & 1)
-            res = res * x % m;
-        x = x * x % m;
-        n >>= 1;
-    }
-    return res;
-}
-inline void fastio()
-{
-    ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-}
-
+template <typename T1, typename T2>
+inline T1 max(T1 a, T2 b) { return a < b ? b : a; }
+template <typename T1, typename T2>
+inline T1 min(T1 a, T2 b) { return a < b ? a : b; }
 void solve();
 signed main()
 {
@@ -101,21 +56,16 @@ signed main()
     clock_t clock_start_time;
     clock_start_time = clock();
 #endif
-
-    read(TESTCASE_NUM);
+    // read(TESTCASE_NUM);
+    // while (getchar() != '\n');
     while (TESTCASE_NUM--)
         solve();
 #ifdef _DEBUG
-    write(endl, "Execution time:", clock() - clock_start_time, "ms", endl, endl);
+    write("\n=> Execution time: ", clock() - clock_start_time, " ms\n\n");
 #endif
     return 0;
 }
 
-//////// Solution Below ////////
-
-ll a, b;
 void solve()
 {
-    read(a, b);
-    write(a + b, endl);
 }
